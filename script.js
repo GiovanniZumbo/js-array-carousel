53fcb3fc6e038e178515cfa57b5d717282425f04
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-20 15:51:33 +0200

    fixed buttons

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // PLANNING
 
-/* */
+/*
+Recuperare gli elementi dal DOM
+Creare un array con le immagini
+Inserire le immagini dinamicamente
+Far funzionare il carosello attraverso i bottoni
+*/
 
 // # STAGING
 
+//* Recuperare gli elementi dal DOM
+
+const gallery = document.getElementById('gallery');
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+
+//* Creare un array con le immagini
+
+const images = []
+
 // # EVENT HANDLING
 
+
 // # PROCESSING
+
 
 // # OUTPUT 
